7cbfdad28ef339ae1ed71170c43b11226b202542
Roman <48863943+Roman351@users.noreply.github.com>
2020-02-10 21:45:38 +0300

    Create sunucuyagirincemesajatmak.js

---
+++ sunucuyagirincemesajatmak.js
@@ -0,0 +1,6 @@
+client.on('guildCreate', guild => {
+let porno = guild.channels.filter(sex => sex.type === "text").random()
+    let roman = porno
+    roman.send("**RomanBot** Sunucunuza Eklendi.. \n ?kurlum yazarak işlem başlatılabilir \n Eğer Herşey zorsa `https://discord.gg/dp5z2tA` Adresine gelerek yardım alabilirsin");
+
+});
