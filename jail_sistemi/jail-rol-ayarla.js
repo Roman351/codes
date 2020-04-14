33f270050cbdb537707e08659a979471a9ae9c7e
Roman <48863943+Roman351@users.noreply.github.com>
2020-04-14 16:24:55 +0300

    Update jail-rol-ayarla.js

--- jail_sistemi/jail-rol-ayarla.js
+++ jail_sistemi/jail-rol-ayarla.js
@@ -5,6 +5,10 @@ const db = require('quick.db')
 
 module.exports.run = async (client, message, args) => {
 let guild = message.guild    
+let roman = await db.fetch(`jailyetkilisi_${message.guild.id}`)
+let yetkili = message.guild.roles.get(roman)
+if(!yetkili) return message.channel.send(`Jail yetkilisi ayarlı değil.`)
+if (!message.member.roles.has(`${yetkili.id}`)) return message.channel.send(`komutu kullanabilmek için ${yetkili} rolüne sahip olman gerekiyor.`)
 let mesaj = args.slice(0).join(" ");
 if (!mesaj) return message.channel.send("Lütfen Bir Rol İsmi Yaz! (Etiket Değil)")
 
