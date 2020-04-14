9bcfd151d6438e913ea03efc0d487435df751614
Roman <48863943+Roman351@users.noreply.github.com>
2020-04-14 16:25:26 +0300

    Update jaillogkanalayarla.js

--- jail_sistemi/jaillogkanalayarla.js
+++ jail_sistemi/jaillogkanalayarla.js
@@ -4,7 +4,10 @@ const db = require('quick.db')
 
 
 module.exports.run = async (client, message, args) => {
-
+let roman = await db.fetch(`jailyetkilisi_${message.guild.id}`)
+let yetkili = message.guild.roles.get(roman)
+if(!yetkili) return message.channel.send(`Jail yetkilisi ayarlı değil.`)
+if (!message.member.roles.has(`${yetkili.id}`)) return message.channel.send(`komutu kullanabilmek için ${yetkili} rolüne sahip olman gerekiyor.`)
  let kanal = message.mentions.channels.first() || message.guild.channels.find(c => c.name === args[1].join('-'))
   if (!kanal) return message.channel.send(` Bir kanal etiketle.`)
 
@@ -25,3 +28,4 @@ module.exports.help = {
   name: 'jail-log-ayarla',
   description: 'Botta bulunan tüm komutları gösterir',
 };
+
