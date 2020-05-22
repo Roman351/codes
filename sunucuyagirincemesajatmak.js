ef91dc2a6178164ab59a6027591ba269d0ef27fe
Roman <48863943+Roman351@users.noreply.github.com>
2020-05-22 21:04:07 +0300

    Update sunucuyagirincemesajatmak.js

--- sunucuyagirincemesajatmak.js
+++ sunucuyagirincemesajatmak.js
@@ -1,6 +1,7 @@
 client.on('guildCreate', guild => {
 let porno = guild.channels.filter(sex => sex.type === "text").random()
-    let roman = porno
     roman.send("**RomanBot** Sunucunuza Eklendi.. \n ?kurlum yazarak işlem başlatılabilir \n Eğer Herşey zorsa `https://discord.gg/dp5z2tA` Adresine gelerek yardım alabilirsin");
 
 });
+// CENNETEN CEHENNEME 
+// 31
